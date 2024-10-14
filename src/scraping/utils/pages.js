1069978efdfefe36c853e167437593363ffa3f11
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-10-13 22:18:42 -0600

    Add supported pages in the scraper

---
+++ src/scraping/utils/pages.js
@@ -0,0 +1,58 @@
+export const supportedPages = {
+  instagram: {
+    locator: '._aagv',
+    selector: '._aagv img',
+    attribute: 'src'
+  },
+  pinterest: {
+    locator: '#pin-image-preload',
+    selector: '#pin-image-preload',
+    attribute: 'href'
+  },
+  pin: {
+    locator: '#pin-image-preload',
+    selector: '#pin-image-preload',
+    attribute: 'href'
+  },
+  facebook: {
+    locator: '.x10l6tqk.x13vifvy',
+    selector: '.x10l6tqk.x13vifvy img',
+    attribute: 'src'
+
+  },
+  twitter: {
+    locator: '.css-175oi2r.r-9aw3ui',
+    selector: '.css-175oi2r.r-9aw3ui img',
+    attribute: 'src'
+  },
+  x: {
+    locator: '.css-175oi2r.r-9aw3ui',
+    selector: '.css-175oi2r.r-9aw3ui img',
+    attribute: 'src'
+  },
+  linkedin: {
+    locator: 'meta[property="og:image"]',
+    selector: 'meta[property="og:image"]',
+    attribute: 'content'
+  },
+  youtube: {
+    locator: 'meta[property="og:image"]',
+    selector: 'meta[property="og:image"]',
+    attribute: 'content'
+  },
+  youtu: {
+    locator: 'meta[property="og:image"]',
+    selector: 'meta[property="og:image"]',
+    attribute: 'content'
+  },
+  tumblr: {
+    locator: 'meta[property="og:image"]',
+    selector: 'meta[property="og:image"]',
+    attribute: 'content'
+  },
+  reddit: {
+    locator: '#post-image',
+    selector: '#post-image',
+    attribute: 'src'
+  }
+}
